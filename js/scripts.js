ef78007225826bd6e1b75f12e5b94a042fd9c7a5
DianaBRamirez <dianabramirez29@gmail.com>
2024-02-07 10:44:30 -0600

    finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 let buscador = document.getElementById("search");
-
 buscador.addEventListener("keyup", function() {
     let cont = 0;
-
+    let searchTerm = new RegExp(buscador.value, 'gi'); // búsqueda insensible a mayúsculas y minúsculas
+    
     Array.from(document.getElementsByTagName("p")).forEach(elemen => {
         const htmlOriginal = elemen.getAttribute("data-original-text") || elemen.innerText;
-
-        if (elemen.innerText.includes(buscador.value) && buscador.value !== '') {
-            const htmlNuevo = htmlOriginal.replaceAll(buscador.value, '<span style="background-color: yellow;">'+buscador.value+'</span>');
+        
+        if (searchTerm.test(htmlOriginal) && buscador.value !== '') {
+            // Realizar la sustitución del texto con la clase CSS
+            const htmlNuevo = htmlOriginal.replace(searchTerm, '<span style="background-color: yellow;">$&</span>');
             elemen.innerHTML = htmlNuevo;
             cont = cont + 1;
         } else {
-            // Restaurar el texto original si el valor del buscador está vacío
             elemen.innerHTML = htmlOriginal;
         }
     });
 });
-
-
